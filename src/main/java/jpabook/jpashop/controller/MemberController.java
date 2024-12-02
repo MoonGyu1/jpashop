1eedd019058b0dd65cd96cc041449d74e91c2495
gyuwonMoon <s201801402@hufs-gsuite.kr>
2024-12-02 21:52:50 +0900

    [Sec8] 웹 계층 개발 - 회원 등록

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        // model을 통해 컨트롤러에서 뷰로 넘어갈 때 데이터를 넘겨줌
+        // 현재의 경우 빈 객체를 넘기는 이유는 밸리데이션을 위함
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    // @Valid: MemberForm에 정의해둔 밸리데이션 어노테이션 검증
+    // BindingResult: 에러가 발생해도 result에 에러 담은 채로 코드 실행 -> 에러 핸들링 가능
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "members/createMemberForm"; // 스프링이 타임리프와 결합해서 에러메세지 띄우도록 렌더링해줌
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
