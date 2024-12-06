5a4884962c9a99b83e865d6f3a1a94b95fb62a48
gyuwonMoon <s201801402@hufs-gsuite.kr>
2024-12-06 15:03:58 +0900

    [Sec5] 컬렉션 조회 최적화 - V6

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -83,4 +83,16 @@ public class OrderQueryRepository {
                                 " join o.delivery d", OrderQueryDto.class)
                 .getResultList();
     }
+
+    // V6 - 쿼리 1번으로 조회
+    public List<OrderFlatDto> findOrderByDto_flat() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d" +
+                                " join o.orderItems oi " +
+                                " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
